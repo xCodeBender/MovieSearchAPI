9e230b3fe09e21324cb92c6340796af7c7c20655
kalai <skalaiarasy@gmail.com>
2021-05-16 14:58:35 -0600

    First commit

---
+++ MovieAPI/Controllers/HomeController.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MovieAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private MovieDAL movieDAL = new MovieDAL();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult MovieSearch()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult MovieSearch(string Title = "")
+        {
+            MovieModel movie = new MovieModel();
+
+            if (Title != "")
+            {
+                movie = movieDAL.ConvertJSONtoSingleTitleMovieModel(Title);
+                return View(movie);
+            }
+
+            else
+            {
+                return RedirectToAction("MovieSearch");
+            }
+        }
+       
+        public IActionResult MovieNight()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult MovieNight(string Title1 , string Title2 , string Title3 )
+        {
+            
+            if (string.IsNullOrEmpty(Title1) && string.IsNullOrEmpty(Title1) && string.IsNullOrEmpty(Title1))
+            {
+                return View();
+            }
+            List<string> movieList = new List<string>
+            {
+                new string(Title1),
+                new string(Title2),
+                new string(Title3)
+            };
+            List<MovieModel> movieResult = new List<MovieModel>();
+
+            if (movieList != null)
+            {
+                foreach (string movie in movieList)
+                {
+                    var movieDetails = movieDAL.ConvertJSONtoSingleTitleMovieModel(movie);
+                    movieResult.Add(movieDetails);
+                }
+
+                return View(movieResult);
+            }
+            else
+            {
+                return RedirectToAction("MovieNight");
+            }
+
+        }
+        
+        //public IActionResult MovieNightMethod(string title1 = "", string title2 = "", string title3 = "")
+        //{
+        //    MovieModel movie = new MovieModel();
+        //    List<MovieModel> movieList = new List<MovieModel>();
+
+        //    if (title1 != "")
+        //    {
+        //        movie = movieDAL.ConvertJSONtoSingleTitleMovieModel(title1);
+        //        movieList.Add(movie);
+        //        return View(movieList);
+        //    }
+        //    if (title2 != "")
+        //    {
+        //        movie = movieDAL.ConvertJSONtoSingleTitleMovieModel(title2);
+        //        movieList.Add(movie);               
+        //        return View(movieList);
+        //    }
+        //    if (title3 != "")
+        //    {
+        //        movie = movieDAL.ConvertJSONtoSingleTitleMovieModel(title3);
+        //        movieList.Add(movie);
+        //        return View(movieList);
+
+        //    }
+        //    //return View(movieList);
+        //    else
+        //    {
+        //        return View(RedirectToAction("MovieNight"));
+        //    }
+
+
+        //}
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MovieAPI/Models/MovieDAL.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace MovieAPI.Models
+{
+    public class MovieDAL
+    {
+    
+        public string GetMovieTitleFromJson(string title)
+        {
+            //make request to api
+            string key = "4a45a52c";
+            string url = $"http://www.omdbapi.com/?apikey={key}&t={title}";
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+            //get and store responds from api
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            //convert the raw Json to string
+
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string JSON = reader.ReadToEnd();
+            return JSON;
+        }
+
+
+
+        //convertion - string to object
+        public MovieModel ConvertJSONtoSingleTitleMovieModel(string Title)
+        {
+            string rawJSON = GetMovieTitleFromJson(Title);
+            MovieModel movie = JsonConvert.DeserializeObject<MovieModel>(rawJSON);
+            return movie;
+        }
+
+        
+    }
+}
